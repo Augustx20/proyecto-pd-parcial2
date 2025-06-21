d9888d39a2b19a88abe5abd66fc5614e7a5d9ca1
augusto <augus1726@gmail.com>
2025-06-21 11:58:38 -0300

    conexion de app.jsx con las pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import { Routes,Route } from 'react-router-dom'
 import './App.css'
-import NavBar from './components/NavBar'
+import Home from './pages/Home'
 
 function App() {
 
 
   return (
-    <>
-      <NavBar/>
-    </>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/login' element={<Login/>}></Route>
+      <Route path='/Dashboard' element={<Dashboard/>}></Route>
+    </Routes>
   )
 }
 
