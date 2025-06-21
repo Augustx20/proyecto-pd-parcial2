8517855589ff3cc3b7acbeff460c61e9458c7b3f
augusto <augus1726@gmail.com>
2025-06-21 11:52:13 -0300

    armado de paginas y login

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 function NavBar() {
   return (
     <div>
-      
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
     </div>
   )
 }
