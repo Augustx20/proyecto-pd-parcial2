552cb6a9d77ca8401360237016a9fd6495e81076
augusto <augus1726@gmail.com>
2025-06-23 22:22:10 -0300

    Diseno de App.jxs sesiones abiertas

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,29 @@ import Peliculas from "./pages/Peliculas";
 import Reservas from "./pages/Reservas";
 import Login from "./pages/Login";
 import AdminPanel from "./pages/AdminPanel";
+import RutaPrivada from "./RutaPrivada";
+import RutaAdmin from "./RutaAdmin";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/peliculas" element={<Peliculas />} />
-        <Route path="/reservas" element={<Reservas />} />
-        <Route path="/adminpanel" element={<AdminPanel />} />
+        <Route path="/peliculas" element={
+          <RutaPrivada>
+            <Peliculas />
+          </RutaPrivada>
+        } />
+        <Route path="/reservas" element={
+          <RutaPrivada>
+            <Reservas />
+          </RutaPrivada>
+        } />
+        <Route path="/adminpanel" element={
+          <RutaAdmin>
+            <AdminPanel />
+          </RutaAdmin>
+        } />
       </Routes>
     </BrowserRouter>
   );
